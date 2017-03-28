0eede2764e6214281ee68242bb8bb1ea58517ce6
kfrn <knfrances@gmail.com>
2017-03-29 12:47:40 +1300

    Remove clutter and slight refactor

--- downloadissue.py
+++ downloadissue.py
@@ -17,14 +17,19 @@ for row in data:
 mag_issues = list(set(mag_issues))
 # print(mag_issues)
 
-def download_mag_issue(input):
-
+def getDownloadURL(input):
     # Ignore blanks, header row, and any random values passed as inputs
-    if "picture" or "Picture" in input:
-        url = "https://archive.org/details/" + input
+    if "picture" in input or "Picture" in input:
+        return "https://archive.org/details/" + input
     else:
         print("Input doesn't contain the relevant string. Exiting")
+
+def download_mag_issue(input):
+    url = getDownloadURL(input)
+    if not url:
         return
+    else:
+        print(url)
 
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -49,60 +54,22 @@ def download_mag_issue(input):
     full_dl_link = "https://archive.org" + download_path
 
     print("Downloading and unzipping %s. This will take a while. Stand by ..." % full_dl_link)
-    #
+
     # with urlopen(full_dl_link) as zip_response:
     #     with ZipFile(BytesIO(zip_response.read())) as zfile:
     #         zfile.extractall('./images/%s_jp2' % input)
     #
     # print("Download and extraction complete. Check the images/%s_jp2 subfolder." % input)
 
-# Do the things
-# for issue in mag_issues:
-#     # Download issue
-#     download_mag_issue(issue)
-#     # Check if there's an image folder for that issue. Obviously there should be
-#     if (os.path.isdir('./images/%s_jp2' % issue)):
-#         print("The subfolder %s_jp2 exists!" % issue)
-#         # Get into that folder. Actual jp2 folder is down a level and not always the same name :(
-#         # Then parse csv. If that issue ID is in column X, copy the file in column y to a new folder.
-#     else:
-#         print("The subfolder %s_jp2 does NOT exist ... nor should it. Exiting")
-#         return
 
 # TESTING ..... #
-
 issue = "Picture-playMagazineJan.1922"
+# issue = "obviously-fake"
 # issue = "pictureplayweekl01unse"
 download_mag_issue(issue)
-outer_img_path = './images/%s_jp2/' % issue
 
-if not os.path.isdir(outer_img_path): # Does that path/dir exist
-    print("The subfolder %s_jp2 does NOT exist ... nor should it. Exiting")
-else:
-    inner_img_folder = os.listdir(outer_img_path)[0] # Get the folder inside
-    path_to_imgs = "%s%s" % (outer_img_path, inner_img_folder)
-    # print("JP2s are located in %s" % path_to_imgs)
-    # Then parse csv. If that issue ID is in column X, copy the file in column y to a new folder.
-    csv_data = csv.reader(open('./data/pictureplay_data.csv', 'r'))
-    for row in csv_data:
-        vol = row[0]
-        no = row[1]
-        if row[2] != "":
-            day = row[2]
-        else:
-            day = "day"
-        month = row[3]
-        year = row[4]
-        jp2_img = row[7]
-        jp2_filename = jp2_img.split('.')[0]
-        for jp2_img in row:
-            if "jp2" in jp2_img: # and look by issue
-                print("original image is located in", path_to_imgs)
-                print("Original image is", jp2_img)
-                output_filename = jp2_filename + "_vol" + vol + "_no" + no + "_" + day + "-" + month + "-" + year + ".jp2"
-                print("Output filename is", output_filename)
-                print("copying ....")
-                print("file located at %s/%s" % (path_to_imgs, jp2_img))
-                # shutil.copy2("%s/%s" % (path_to_imgs, jp2_img), "./jp2_images/%s" % output_filename)
-            # else:
-                # print("bzzzz. wrong")
+# # Actual call
+# for issue in mag_issues:
+#     download_mag_issue(issue)
+
+# outer_img_path = './images/%s_jp2/' % issue
