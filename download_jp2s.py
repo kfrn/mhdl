5f0ac0bbeb519aead86b4a28bfab71140d6705a7
kfrn <knfrances@gmail.com>
2017-03-30 09:20:58 +1300

    Refactor

--- download_jp2s.py
+++ download_jp2s.py
@@ -17,15 +17,33 @@ for row in data:
 mag_issues = list(set(mag_issues))
 # print(mag_issues)
 
-def get_download_url(input):
+def get_download_page(input):
     # Ignore blanks, header row, and any random values passed as inputs
     if "picture" in input or "Picture" in input:
         return "https://archive.org/details/" + input
     else:
         print("Input doesn't contain the relevant string. Exiting")
 
+def get_download_path(links):
+    if len(links) == 0:
+        print("No relevant download links found. Exiting")
+        return
+    elif len(links) == 1:
+        return "https://archive.org" + links[0]
+    elif len(links) == 2:
+        return "https://archive.org" + links[1]
+    else:
+        print("More than two jp2-related downloads? That's odd. You should check that out.")
+        return
+
+def unzip_folder(path_to_folder, input):
+    with urlopen(path_to_folder) as zip_response:
+        with ZipFile(BytesIO(zip_response.read())) as zfile:
+            zfile.extractall('./images/%s_jp2' % input)
+    print("Download and extraction complete. Check the images/%s_jp2 subfolder." % input)
+
 def download_mag_issue(input):
-    url = get_download_url(input)
+    url = get_download_page(input)
     if not url:
         return
 
@@ -38,46 +56,29 @@ def download_mag_issue(input):
         download_links.append(dlink.get('href'))
     download_links = [x for x in download_links if 'jp2' in x]
 
-    if len(download_links) == 0:
-        print("No relevant download links found. Exiting")
-        return
-    elif len(download_links) == 1:
-        download_path = download_links[0]
-    elif len(download_links) == 2:
-        download_path = download_links[1]
-    else:
-        print("More than two jp2-related downloads? That's odd. You should check that out.")
+    full_dl_path = get_download_path(download_links)
+    if not full_dl_path:
         return
 
-    full_dl_link = "https://archive.org" + download_path
+    print("Downloading and unzipping %s. This will take a while. Stand by ..." % full_dl_path)
+    # unzip_folder(full_dl_path, input)
 
-    print("Downloading and unzipping %s. This will take a while. Stand by ..." % full_dl_link)
-
-    # with urlopen(full_dl_link) as zip_response:
-    #     with ZipFile(BytesIO(zip_response.read())) as zfile:
-    #         zfile.extractall('./images/%s_jp2' % input)
-    #
-    # print("Download and extraction complete. Check the images/%s_jp2 subfolder." % input)
 
 def does_imgfolder_exist(img_path):
     return os.path.isdir(img_path)
 
+def get_path_to_imgs(outer_folder):
+    return outer_folder + os.listdir(outer_folder)[0]
+
 def copyRelImageFiles(input):
     outer_img_folder = './images/%s_jp2/' % issue
 
-    # does the image folder exist?
     if not does_imgfolder_exist(outer_img_folder):
         print("There's no image folder related to that issue. Exiting")
         return
 
-    # get the inner image folder, i.e. actual image location.
-    inner_img_folder = os.listdir(outer_img_folder)[0]
-    path_to_imgs = "%s%s" % (outer_img_folder, inner_img_folder)
+    path_to_imgs = get_path_to_imgs(outer_img_folder)
 
-    print("Path to images is %s" % path_to_imgs)
-
-    # Then parse csv.
-    # Where the issue name ('issue') matches the entry in the BookID column ...
     csv_data = csv.reader(open('./data/pictureplay_data.csv', 'r'))
     for row in csv_data:
         vol = row[0]
@@ -97,9 +98,9 @@ def copyRelImageFiles(input):
 
 
 # TESTING ..... #
-issue = "Picture-playMagazineJan.1922"
+# issue = "Picture-playMagazineJan.1922"
 # issue = "obviously-fake"
-# issue = "pictureplayweekl01unse"
+issue = "pictureplayweekl01unse"
 download_mag_issue(issue)
 copyRelImageFiles(issue)
 
